b2b6bd938221e7922662a46593b3648d73004e3e
Han Gao <rabenda.cn@gmail.com>
2025-12-31 18:42:26 +0800

    New upstream version 0.44.0

--- ruyi/telemetry/aggregate.py
+++ ruyi/telemetry/aggregate.py
@@ -3,8 +3,8 @@ from typing import Iterable, TypeAlias, TypedDict, TYPE_CHECKING
 if TYPE_CHECKING:
     from typing_extensions import NotRequired
 
+from ..utils.node_info import NodeInfo
 from .event import TelemetryEvent
-from .node_info import NodeInfo
 
 
 class AggregatedTelemetryEvent(TypedDict):
@@ -18,8 +18,13 @@ class UploadPayload(TypedDict):
     fmt: int
     nonce: str
     ruyi_version: str
+    report_uuid: "NotRequired[str]"
+    """Optional field in case the client wishes to report this, and nothing
+    else. If `installation` is present, this field is ignored."""
     installation: "NotRequired[NodeInfo | None]"
+    """More detailed installation info that the client has user consent to report."""
     events: list[AggregatedTelemetryEvent]
+    """Aggregated telemetry events that the client has user consent to upload."""
 
 
 def stringify_param_val(v: object) -> str:
